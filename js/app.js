e23c87061474a75295ae7862d03ccb751304a854
dev-rahim <rahimuddin5915@gmail.com>
2023-03-03 18:12:09 +0600

    get only 6 tools from  all data and destructuring done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const loadData = () => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => console.log(data.data.tools));
+    .then((data) => displayData(data.data.tools));
 };
 loadData();
+const displayData = (tools) => {
+  const firstSixTools = tools.splice(0, 6);
+  firstSixTools.forEach((tool) => {
+    const { id, name, image, features, published_in } = tool;
+
+  });
+};
