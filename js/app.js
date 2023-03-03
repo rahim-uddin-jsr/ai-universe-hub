edcc56fd3f4f4d55cf6c423ff10b61d0a440b8b2
dev-rahim <rahimuddin5915@gmail.com>
2023-03-03 20:47:36 +0600

    added some informetion in card

--- js/app.js
+++ js/app.js
@@ -10,24 +10,38 @@ const displayData = (tools) => {
   firstSixTools.forEach((tool) => {
     const { id, name, image, features, published_in } = tool;
     const div = document.createElement("div");
+    const ol = document.createElement("ol");
+    ol.setAttribute("id", "feature-container");
+    for (const feature of features) {
+      const li = document.createElement("li");
+      li.innerText = feature;
+      ol.appendChild(li);
+    }
+    // console.log(ol);
     div.innerHTML = `
         <div class="card  bg-base-100 shadow-xl">
-            <figure class="p-4">
-                <img src="${image}" alt="Shoes" class="rounded-xl" />
+            <figure class="p-6">
+                <img src="${image}" alt="Shoes" class="rounded-xl w-[437px] h-[300px]" />
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Features</h2>
-                <ul>
+            <div id='card-body' class="card-body">
+                <h2 class="card-title text-2xl">Features</h2>
                 
-                </ul>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+                <hr class='my-6'>
+                <div class="flex justify-between items-center">
+                    <div >
+                        <h2 class="card-title text-2xl mb-4">${name}</h2>
+                        <p>
+                        <i class="fa-solid fa-calendar-days"></i> ${published_in}
+                        </p>
+                    </div>
+                    <div class="card-actions justify-end text-end">
+                        <button class="btn hover:text-white text-primary border-0 hover: rounded-full  bg-secondary"><i class="font-extrabold fa-solid fa-arrow-right"></i></button>
+                    </div>
                 </div>
             </div>
         </div>
       `;
-    console.log(5);
     cardContainer.appendChild(div);
+    // document.getElementById("card-body").appendChild(ol);
   });
 };
