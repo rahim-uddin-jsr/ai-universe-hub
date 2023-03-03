70be9f3767d5289e79cfc4a2fa9778e5b7dfc29b
dev-rahim <rahimuddin5915@gmail.com>
2023-03-03 19:17:42 +0600

    shown 6 tools in 6 cards done

--- js/app.js
+++ js/app.js
@@ -4,10 +4,30 @@ const loadData = () => {
     .then((data) => displayData(data.data.tools));
 };
 loadData();
+const cardContainer = document.getElementById("card-container");
 const displayData = (tools) => {
   const firstSixTools = tools.splice(0, 6);
   firstSixTools.forEach((tool) => {
     const { id, name, image, features, published_in } = tool;
-
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="card  bg-base-100 shadow-xl">
+            <figure class="p-4">
+                <img src="${image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Features</h2>
+                <ul>
+                
+                </ul>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+      `;
+    console.log(5);
+    cardContainer.appendChild(div);
   });
 };
