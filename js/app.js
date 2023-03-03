13edf0f8784131adf5fa6d6c5731073945b8c0cb
dev-rahim <rahimuddin5915@gmail.com>
2023-03-03 21:10:04 +0600

    added see more btn and its funconality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
-const loadData = () => {
+const cardContainer = document.getElementById("card-container");
+const loadData = (fistSix = true) => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => displayData(data.data.tools));
+    .then((data) => {
+      if (fistSix) {
+        displayData(data.data.tools.splice(0, 6));
+      } else {
+        cardContainer.textContent = "";
+        displayData(data.data.tools);
+        document.getElementById("btn-see-more").classList.add("hidden");
+      }
+    });
 };
 loadData();
-const cardContainer = document.getElementById("card-container");
 const displayData = (tools) => {
-  const firstSixTools = tools.splice(0, 6);
-  firstSixTools.forEach((tool) => {
+  //   let Tools = tools.splice(0, 6);
+  //   Tools = tools;
+  tools.forEach((tool) => {
     const { id, name, image, features, published_in } = tool;
     const div = document.createElement("div");
     const ol = document.createElement("ol");
@@ -17,7 +26,6 @@ const displayData = (tools) => {
       li.innerText = feature;
       ol.appendChild(li);
     }
-    // console.log(ol);
     div.innerHTML = `
         <div class="card  bg-base-100 shadow-xl">
             <figure class="p-6">
@@ -35,7 +43,7 @@ const displayData = (tools) => {
                         </p>
                     </div>
                     <div class="card-actions justify-end text-end">
-                        <button class="btn hover:text-white text-primary border-0 hover: rounded-full  bg-secondary"><i class="font-extrabold fa-solid fa-arrow-right"></i></button>
+                        <button onClick='' class="btn hover:text-white text-primary border-0 hover: rounded-full  bg-secondary"><i class="font-extrabold fa-solid fa-arrow-right"></i></button>
                     </div>
                 </div>
             </div>
