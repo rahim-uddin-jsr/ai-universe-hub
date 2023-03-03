d319ff5dcaa3346e6321f4e748839f4f2e7b4e76
dev-rahim <rahimuddin5915@gmail.com>
2023-03-03 17:58:35 +0600

    load data from api done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+const loadData = () => {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => console.log(data.data.tools));
+};
+loadData();
